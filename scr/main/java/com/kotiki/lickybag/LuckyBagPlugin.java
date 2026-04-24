19598f0dd4ef6d5e6bec44e190b3674485d53302
Max <zezulko@gmail.com>
2026-04-24 11:51:37 +0300

    Update LuckyBagPlugin.java

--- scr/main/java/com/kotiki/lickybag/LuckyBagPlugin.java
+++ scr/main/java/com/kotiki/lickybag/LuckyBagPlugin.java
@@ -7,11 +7,11 @@ import java.util.Random;
 
 public class LuckyBagPlugin extends PluginBase {
 
-    private Random random = new Random();
+    private final Random random = new Random();
 
     @Override
     public void onEnable() {
-        getLogger().info("LuckyBag enabled");
+        getLogger().info("LuckyBag ENABLED");
 
         getServer().getScheduler().scheduleRepeatingTask(this, () -> {
             check();
@@ -21,17 +21,21 @@ public class LuckyBagPlugin extends PluginBase {
     private void check() {
 
         String[] loot = {
-            "give %player% diamond 3",
-            "give %player% emerald 5",
-            "give %player% iron_ingot 32",
-            "give %player% golden_apple 2"
+                "give %player% diamond 3",
+                "give %player% emerald 5",
+                "give %player% iron_ingot 32",
+                "give %player% gold_ingot 16",
+                "give %player% golden_apple 2"
         };
 
-        String cmd = loot[random.nextInt(loot.length)];
+        String player = "Steve"; // тест (потом заменим на MySQL)
+
+        String cmd = loot[random.nextInt(loot.length)]
+                .replace("%player%", player);
 
         Server.getInstance().dispatchCommand(
-            Server.getInstance().getConsoleSender(),
-            cmd
+                Server.getInstance().getConsoleSender(),
+                cmd
         );
     }
 }
